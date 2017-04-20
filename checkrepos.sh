693159bfb762a2c46d5c07ba2268c03099b76ad5
Ed Bueler <elbueler@alaska.edu>
2017-04-20 10:57:30 -0800

    checkrepos.sh now tighter, faster, clearer

--- checkrepos.sh
+++ checkrepos.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 for GITDIR in `ls -1d */`; do
-  echo
-  echo "checking $GITDIR"
-  (cd $GITDIR && git status)
-  sleep 0.2
+  cd $GITDIR
+  BRANCH=`git status |grep "On branch" |awk '{print $3}'`
+  if git status |grep -q "working tree clean"; then
+      printf "checking $GITDIR [$BRANCH]:  ... clean\n"
+  else
+      printf "\nchecking $GITDIR [$BRANCH]:\n"
+      git status
+  fi
+  cd ..
+  #sleep 0.1
 done
 
