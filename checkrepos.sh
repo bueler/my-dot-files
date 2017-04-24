44f6cedd319ef4a733fbcadd36a955738cc57e6d
Ed Bueler <elbueler@alaska.edu>
2017-04-24 14:06:48 -0800

    git reports clean even if unpushedcommits exist

--- checkrepos.sh
+++ checkrepos.sh
@@ -4,7 +4,13 @@ for GITDIR in `ls -1d */`; do
   cd $GITDIR
   BRANCH=`git status |grep "On branch" |awk '{print $3}'`
   if git status |grep -q "working tree clean"; then
-      printf "checking $GITDIR [$BRANCH]:  ... clean\n"
+      if git status |grep -q "commits"; then
+          printf "\nchecking $GITDIR:\n"
+          git status
+          printf "\n"
+      else
+          printf "checking $GITDIR [$BRANCH]:  ... clean\n"
+      fi
   else
       printf "\nchecking $GITDIR [$BRANCH]:\n"
       git status
