a3c2a02db0d1f7db461d3878ca15b033d821fc85
Ed Bueler <elbueler@alaska.edu>
2022-04-17 15:51:15 -0800

    MNG is just as good as GIF, and much faster

--- printmagick.sh
+++ printmagick.sh
@@ -4,13 +4,13 @@
 #   pdftoppm
 #   convert
 
-# A script to rebuild problematic and un-printable PDFs as 300dpi images,
-# then back to a PDF:
+# A script to rebuild problematic and un-printable PDFs as 300dpi
+# images, then back to a PDF:
 #    ./printmagick.sh in.pdf out.pdf
 
 echo "generating PNG images $1-*.png ..."
 pdftoppm $1 $1 -png -r 300
-echo "converting PNG images to temporary GIF ..."  # VERY SLOW, BUT WORKS?
-convert $1-*.png tmp-out.gif
-echo "converting GIF to multipage PDF $2 ..."
-convert tmp-out.gif $2
+echo "converting PNG images to temporary MNG ..."
+convert $1-*.png tmp-out.mng
+echo "converting MNG to multipage PDF $2 ..."
+convert tmp-out.mng $2
